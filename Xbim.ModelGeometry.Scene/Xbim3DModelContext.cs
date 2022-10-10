f9c0e7a3c5d1a666d1167c53c716cded0462d01a
董士崇 <dongshichong@thape.com.cn>
2022-10-10 13:57:50 +0800

    Revert "关闭XBim扣减功能"
    This reverts commit a662b95d1aaee2c68e79ac73e64f75b6f71e71b1.

--- Xbim.ModelGeometry.Scene/Xbim3DModelContext.cs
+++ Xbim.ModelGeometry.Scene/Xbim3DModelContext.cs
@@ -746,8 +746,7 @@ namespace Xbim.ModelGeometry.Scene
             PerformSubtractions = 2,
             ReplaceBoundingBox = 4,
             Skip = 8,
-            Default = PerformAdditions,
-            //Default = PerformAdditions | PerformSubtractions
+            Default = PerformAdditions | PerformSubtractions
         }
 
         public delegate MeshingBehaviourResult MeshingBehaviourSetter(int elementId, int typeId, ref double linearDeflection,
@@ -1443,8 +1442,8 @@ namespace Xbim.ModelGeometry.Scene
 
                 if (s.ElapsedMilliseconds > 20000)
                 {
-                    LogWarning(shape, "Long meshing time of shape geometry: {0} ms.", s.ElapsedMilliseconds);
-                    Debug.WriteLine($"-\t{s.ElapsedMilliseconds,5}\tms {shape.GetType()}: #{shape.EntityLabel}");
+					LogWarning(shape, "Long meshing time of shape geometry: {0} ms.", s.ElapsedMilliseconds);
+					Debug.WriteLine($"-\t{s.ElapsedMilliseconds,5}\tms {shape.GetType()}: #{shape.EntityLabel}");
                 }
             }
             );
